816978e96d62a754d0794176a6ccfca136077372
Pranav2907 <71582082+Pranav2907@users.noreply.github.com>
2022-01-14 18:32:31 +0530

    day12 completed

---
+++ lib/widgets/item_widget.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: prefer_const_constructors, avoid_print
+
+import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
+
+// ignore: use_key_in_widget_constructors
+class ItemWidget extends StatelessWidget {
+
+   final Item item;
+
+  const ItemWidget({Key? key,  required this.item}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        onTap: (){
+          print("${item.name} pressed");
+        },
+        leading: Image.network(item.image),
+        title:Text(item.name),
+        subtitle: Text(item.desc),
+        trailing: Text("\$${item.price}",
+         style:TextStyle(
+           color: Colors.black,
+           fontWeight: FontWeight.bold,
+         )      
+        ),
+      ),
+    );
+  }
+}

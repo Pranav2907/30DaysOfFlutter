2eea32593689b9cf40d99d07b8f729ef95df5d78
Pranav2907 <71582082+Pranav2907@users.noreply.github.com>
2022-01-18 11:08:15 +0530

    day27 completed

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -32,14 +32,7 @@ late CatalogModel _catalog; /// check this once
   num get totalPrice => items.fold(0, (total, current) => total+current.price);
 
   
-
-  //revmove item
-  void remove(Item item){
-    _itemIds.remove(item.id);
-  }
 }
-
-
 class AddMutation extends VxMutation<MyStore>{
   final Item item ;
   
@@ -50,3 +43,15 @@ class AddMutation extends VxMutation<MyStore>{
     store?.cart._itemIds.add(item.id) ;
   }
 }
+
+class RemoveMutation extends VxMutation<MyStore>{
+  final Item item ;
+  
+
+  RemoveMutation(this.item);
+  @override
+  perform() {
+    store?.cart._itemIds.remove(item.id) ;
+  }
+}
+
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -41,7 +41,12 @@ class _CartTotal extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
         // ignore: deprecated_member_use
-        "\$${_cart.totalPrice}".text.xl5.color(context.theme.accentColor).make(),
+        VxBuilder(
+          mutations: {RemoveMutation},
+          builder: (context, _, status) {
+            return   "\$${_cart.totalPrice}".text.xl5.color(context.theme.accentColor).make();
+          } ,
+        ),
         30.widthBox,
         ElevatedButton(
           onPressed: (){
@@ -62,15 +67,14 @@ class _CartList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    VxState.watch(context, on: [RemoveMutation]);
      final CartModel _cart = (VxState.store as MyStore).cart;
     return _cart.items.isEmpty? "Nothing to show ".text.xl3.makeCentered(): ListView.builder(
       itemCount: _cart.items.length,
       itemBuilder: (context,index) => ListTile(
         leading: Icon(Icons.done),
         trailing: IconButton(icon: Icon(Icons.remove_circle_outline),
-           onPressed: () {
-             _cart.remove(_cart.items[index]);
-           },
+           onPressed: () => RemoveMutation(_cart.items[index]),
         ),
         title:_cart.items[index].name.text.make() ,
       ),
